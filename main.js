03e363796a566bc594aba0478d83976d599df72b
lamsalbishal <bishalrajlamsal65@gamil.com>
2018-03-31 11:52:17 +0545

    This is first page

---
+++ main.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partial');
+app.set('View engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+
+	res.render('home.hbs' , {
+
+		pageTitle: 'Home Page',
+		welcomePage: ' Welcome to Our page',
+		CurrentYear: new Date().getFullYear()
+
+	});
+
+});
+
+// app.get('/about',(req, res) => {
+
+// 	res.send("About Page");
+
+// });
+
+app.get('/about', (req,res) => {
+
+	res.render('about.hbs', { 
+
+		pageTitle: ' About Page',
+		CurrentYear: new Date().getFullYear()
+
+	});
+
+});
+
+app.listen(3000);
+
+console.log("hello world");
